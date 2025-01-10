d2fedf42e41eaa4bf1e4a9e07e07dc683980981b
Maximilian Bischoff <maximilian.bischoff@inovex.de>
2025-01-10 15:16:08 +0100

    add healthz endpoint
    this is exposed on the default port used in the helm chart:
    https://github.com/kubernetes-sigs/external-dns/blob/f8057a715fdeceece61787af52667b64dc326c8e/charts/external-dns/values.yaml#L284

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -1,22 +1,59 @@
 package main
 
 import (
+	"net"
+	"net/http"
+
 	log "github.com/sirupsen/logrus"
+
+	"external-dns-openstack-webhook/internal/designate/provider"
 
 	"sigs.k8s.io/external-dns/endpoint"
 	"sigs.k8s.io/external-dns/provider/webhook/api"
 )
 
-import "external-dns-openstack-webhook/internal/designate/provider"
-
 func main() {
+	log.SetLevel(log.DebugLevel)
+
+	startedChan := make(chan struct{})
+	httpApiStarted := false
+
+	go func() {
+		<-startedChan
+		httpApiStarted = true
+	}()
+
+	m := http.NewServeMux()
+	m.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		if !httpApiStarted {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+
+	go func() {
+		log.Debug("Starting status server on :8080")
+		s := &http.Server{
+			Addr:    "0.0.0.0:8080",
+			Handler: m,
+		}
+
+		l, err := net.Listen("tcp", "0.0.0.0:8080")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = s.Serve(l)
+		if err != nil {
+			log.Fatalf("health listener stopped : %s", err)
+		}
+	}()
+
+
 	epf := endpoint.NewDomainFilter([]string{})
 	dp, err := provider.NewDesignateProvider(epf, false)
 	if err != nil {
 		log.Fatalf("NewDesignateProvider: %v", err)
 	}
 
-	log.SetLevel(log.DebugLevel)
 	log.Printf("Starting server")
-	api.StartHTTPApi(dp, nil, 0, 0, "127.0.0.1:8888")
+	api.StartHTTPApi(dp, startedChan, 0, 0, "127.0.0.1:8888")
 }
