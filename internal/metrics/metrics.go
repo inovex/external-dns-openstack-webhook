d29f6056abcc11950f42ee1d1a2ad660fc2ea762
Nico <nico-j@gmx.net>
2025-08-21 14:41:30 +0200

    ✨ feat(metrics): update OpenStack connection metric
    - rename metric to indicate initialization with API credentials
    - clarify help text for better understanding of connection status

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -6,8 +6,8 @@ import (
 
 var (
 	OpenstackConnectionMetric = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "external_dns_webhook_openstack_connection",
-		Help: "Indicates if the webhook has a connection to the OpenStack API (1 for connected, 0 for not connected)",
+		Name: "external_dns_webhook_openstack_connection_initialized",
+		Help: "Indicates if the webhook has been initialized with OpenStack API credentials (1 for initialized, 0 for not initialized)",
 	})
 	FailedApiCallsTotal = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "external_dns_webhook_failed_api_calls_total",
